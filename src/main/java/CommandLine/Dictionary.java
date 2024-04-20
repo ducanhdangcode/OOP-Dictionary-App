4be5cc631b290af610c7df9693007feb878c0646
Quach Le Hai Anh <haianhlequach@gmail.com>
2024-04-20 15:02:15 +0700

    update CommandLine

---
+++ src/main/java/CommandLine/Dictionary.java
@@ -0,0 +1,153 @@
+package CommandLine;
+
+import java.util.ArrayList;
+
+public class Dictionary {
+    private ArrayList<Word> wordList = new ArrayList<>();
+
+    public Dictionary() {
+        this.wordList = new ArrayList<>();
+    }
+
+    /**
+     * Display wordList.
+     */
+    public void displayWords() {
+        System.out.println("No   |   English         |   Vietnamese");
+        System.out.println("--------------------------------------------");
+        for (int i = 0; i < wordList.size(); ++i) {
+            System.out.printf("%-4d |   %-15s |   %-15s%n", (i + 1), wordList.get(i).getWordTarget(), wordList.get(i).getWordExplain());
+        }
+    }
+
+    /**
+     * Get words.
+     *
+     * @return wordList
+     */
+    public ArrayList<Word> getWords() {
+        return wordList;
+    }
+
+    /**
+     * Set words.
+     *
+     * @param words wordList
+     */
+    public void setWords(ArrayList<Word> words) {
+        this.wordList = words;
+    }
+
+    /**
+     * add new word.
+     *
+     * @param word new word
+     */
+    public void addWord(Word word) {
+        int length = wordList.size();
+        int index = searchIndexInsert(0, length - 1, word.getWordTarget());
+        if (index <= length && index >= 0) {
+            wordList.add(index, word);
+        } else {
+            System.out.println("The word already exists");
+        }
+    }
+
+    /**
+     * remove E word.
+     *
+     * @param wordTarget E word
+     */
+    public void removeWord(String wordTarget) {
+        int index = binarySearchWord(wordTarget);
+        if (index >= 0) {
+            wordList.remove(index);
+            System.out.println("Remove successfully!");
+        } else {
+            System.out.println("Word not found, no word removed.");
+        }
+    }
+
+    /**
+     * update word meaning.
+     *
+     * @param wordTarget        word in English
+     * @param updateWordExplain word meaning
+     */
+    public void updateWord(String wordTarget, String updateWordExplain) {
+        for (Word word : wordList) {
+            if (word.getWordTarget().equals(wordTarget)) {
+                word.setWordExplain(updateWordExplain);
+                System.out.println("Update successfully!");
+                return;
+            }
+        }
+        System.out.println("Word not found, update word failed!");
+    }
+
+    /**
+     * Search word index.
+     *
+     * @param start      start index
+     * @param end        end index
+     * @param wordTarget word in English
+     * @return index
+     */
+    private int searchIndexInsert(int start, int end, String wordTarget) {
+        if (end < start) return start;
+        int mid = start + (end - start) / 2;
+        if (mid == wordList.size()) return mid;
+        Word word = wordList.get(mid);
+        int compare = word.getWordTarget().compareTo(wordTarget);
+        if (compare == 0) return -1;
+        if (compare > 0) return searchIndexInsert(start, mid - 1, wordTarget);
+        return searchIndexInsert(mid + 1, end, wordTarget);
+    }
+
+    /**
+     * Find index of word.
+     *
+     * @param wordTarget word in English
+     * @return index or -1 if not found
+     */
+    private int binarySearchWord(String wordTarget) {
+        wordList.sort(new Sort());
+        int left = 0;
+        int right = wordList.size() - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int compareResult = wordList.get(mid).getWordTarget().compareTo(wordTarget);
+            if (compareResult == 0) {
+                return mid;
+            } else if (compareResult < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Look up.
+     *
+     * @param wordTarget word in English
+     * @return mean in Vietnamese
+     */
+    public String lookupWord(String wordTarget) {
+        int index = binarySearchWord(wordTarget);
+        if (index >= 0) {
+            return "Work Explain: " + wordList.get(index).getWordExplain();
+        }
+        return "Work not found, lookup word failed!";
+    }
+
+    /**
+     * Get word at index i.
+     *
+     * @param index index
+     */
+    public Word getWord(int index) {
+        return wordList.get(index);
+    }
+}
---
+++ src/main/java/CommandLine/DictionaryCommandLine.java
@@ -0,0 +1,129 @@
+package CommandLine;
+
+import java.util.Scanner;
+
+public class DictionaryCommandLine {
+    private Dictionary dictionary;
+    public DictionaryCommandLine(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public void showAllWords() {
+        dictionary.displayWords();
+    }
+
+    public void dictionaryBasic() {
+        DictionaryManagement management = new DictionaryManagement(dictionary);
+        management.insertFromCommandLine();
+        showAllWords();
+    }
+
+    public Dictionary getDictionary() {
+        return dictionary;
+    }
+
+    public void setDictionary(Dictionary dictionary) {
+        this.dictionary = dictionary;
+    }
+
+    public void displayAdvance() {
+        DictionaryManagement management = new DictionaryManagement(dictionary);
+        Scanner sc = new Scanner(System.in);
+        management.insertFromFile("src/main/resources/data/dictionaries.txt");
+        System.out.println("Welcome to My Application!");
+        while (true) {
+            System.out.println("[0] Exit");
+            System.out.println("[1] Add");
+            System.out.println("[2] Remove");
+            System.out.println("[3] Update");
+            System.out.println("[4] Display");
+            System.out.println("[5] Lookup");
+            System.out.println("[6] Search");
+            System.out.println("[7] Game");
+            System.out.println("[8] Import from file");
+            System.out.println("[9] Export to file");
+            System.out.println("[10] Mini game");
+            System.out.print("Your action: ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 0:
+                    System.out.println("Goodbye!");
+                    return;
+                case 1:
+                    management.insertFromCommandLine();
+                    break;
+                case 2:
+                    System.out.print("Enter a word to remove: ");
+                    String wordToRemove = sc.nextLine();
+                    wordToRemove = wordToRemove.trim().toLowerCase();
+                    management.removeWord(wordToRemove);
+                    break;
+                case 3:
+                    System.out.print("Enter a word to update: ");
+                    sc.nextLine();
+                    System.out.print("English: ");
+                    String wordTarget = sc.nextLine();
+                    System.out.print("Vietnamese: ");
+                    String updatedWordExplain = sc.nextLine();
+                    management.updateWord(wordTarget, updatedWordExplain);
+                    break;
+                case 4:
+                    showAllWords();
+                    break;
+                case 5:
+                    System.out.print("Enter a word to lookup: ");
+                    String wordSearch = sc.nextLine();
+                    System.out.println(management.dictionaryLookup(wordSearch));
+                    break;
+                case 6:
+                    System.out.print("Enter a prefix to search: ");
+                    String searchPrefix = sc.nextLine();
+                    management.searchByPrefix(searchPrefix);
+                    break;
+                case 7:
+                    System.out.println("You have 5 turns to guess the meaning of the word");
+                    System.out.println("Type C - Enter to Continue or ESC-Enter to Exit");
+                    Scanner choose = new Scanner(System.in);
+                    System.out.println("Your choice: ");
+                    String option;
+                    option = choose.nextLine();
+                    if (option.equals("C")) {
+                        Game gm = new Game();
+                        while (!option.equals("ESC")) {
+                            Game plGame = new Game();
+                            Word  targetWord = plGame.getRandomWord(this.dictionary);
+                            plGame.playGame(targetWord);
+                            System.out.println("Type C - Enter to Continue or ESC-Enter to Exit");
+                            option = sc.nextLine();
+                        }
+
+                    }
+                    else {
+                        break;
+                    }
+                    break;
+                case 8:
+                    management.exportToFile(dictionary);
+                    System.out.println("Import to file succesfully!");
+                    break;
+                case 9:
+                    management.exportToFile(dictionary);
+                    System.out.println("Export to file succesfully!");
+                    break;
+                case 10:
+                    System.out.println("You have 5 turns to guess the meaning of the word");
+                    break;
+                default:
+                    System.out.println("Action not supported.");
+                    break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Dictionary dic = new Dictionary();
+        DictionaryCommandLine test = new DictionaryCommandLine(dic);
+        test.displayAdvance();
+    }
+}
