92278d58e0f61e9dd71bb2253feabc2542176cc3
ducanhdangcode <161180719+ducanhdangcode@users.noreply.github.com>
2024-05-02 15:37:57 +0700

    Delete src directory

--- src/main/java/com/example/superdictionaryproject/SearchingController.java
+++
@@ -1,6 +0,0 @@
-package com.example.superdictionaryproject;
-
-public class SearchingController {
-
-
-}
--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.superdictionaryproject {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-
-    opens com.example.superdictionaryproject to javafx.fxml;
-    exports com.example.superdictionaryproject;
-}
